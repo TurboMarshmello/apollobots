7b86e38c27986aade3c47e7941ec8072135371ec
Dave R <davetheprogrammer2@gmail.com>
2017-07-02 09:34:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <title>Apollo</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="description" content="A Cinema Site for trailers">
-  <meta name="keywords" content="bots, discord, servers, list, mello, turbomarshmello">
+  <meta name="description" content="A Discord Bot list!">
+  <meta name="keywords" content="bots, discord, servers, list, mello, turbomarshmello, ohlookitsderpy, derpy, noahliedfilm, noah">
   <meta name="author" content="Turbomarshmello">
   <link rel="stylesheet" href="CSS/style.css">
      <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,16 +23,16 @@
   <div class="row">
     <div class="col-sm-4">
       <h3>Bots</h3>
-      <p>We encorage all bots unless they are clones of others</p>
-      <p>If they are clones they will not be accepeted</p>
+      <p>We encourage all bots unless they are clones of others, or if they contain NSFW</p>
+      <p>If they are clones/contain NSFW they will not be accepeted</p>
     </div>
     <div class="col-sm-4">
-      <h3>Column 2</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+      <h3>Website</h3>
+      <p>This website is a work-in-progress</p>
+      <p>The website will get updates soom</p>
     </div>
     <div class="col-sm-4">
-      <h3>Column 3</h3>        
+      <h3>Stuff</h3>        
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
       <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
